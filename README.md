066854322359249c0f29a947ce67c048cda36ac6
markus-neubrand <markus.neubrand@salesforce.com>
2025-03-18 14:06:09 +0100

    Initial Heroku Integtration app

---
+++ README.md
@@ -0,0 +1,4 @@
+# Heroku Integration Node.js App Template
+Heroku Integration Node.js app template is an app the employs the [Fastify](https://fastify.dev/) web framework.
+
+See [Getting Started with Heroku Integration](https://devcenter.heroku.com/articles/getting-started-heroku-integration).
---
+++ src/routes/index.js
@@ -0,0 +1,61 @@
+'use strict'
+
+const yaml = require('yaml');
+const fetch = require('node-fetch');
+
+module.exports = async function (fastify, opts) {
+
+  /**
+   * Receives a payload containing a Salesforce Account ID, retrieves a yaml file of transactions from an external system
+   * and writes transactions belonging to the account back to the calling salesforce org
+   */
+  fastify.post('/parse-transactions', async function (request, reply) {
+    const {event, context, logger} = request.sdk;
+    const org = context.org;
+    const dataApi = context.org.dataApi;
+
+    logger.info(`POST /parse-transactions ${JSON.stringify(event.data || {})}`);
+
+    const validateField = (field, value) => {
+      if (!value) throw new Error(`Please provide ${field}`);
+    }
+
+    // Validate Input
+    const data = event.data;
+    validateField('accountId', data.accountId);
+
+    // Parse YAML
+    const yamlText = await fetch('https://raw.githubusercontent.com/markusneubrand-sf/transation-data/refs/heads/main/transactions-sample.yaml')
+                          .then((response) => response.text());
+    const parsed = yaml.parse(yamlText);
+
+    // Create a unit of work that inserts transactions belonging to the account
+    const uow = dataApi.newUnitOfWork();
+    for (var i = 0; i < parsed.length; i++) {
+      if (parsed[i].accountId == data.accountId) {
+        logger.info(JSON.stringify(response));
+        uow.registerCreate({
+          type: 'Transaction__c',
+          fields: {
+            Account__c: parsed[i].accountId,
+            Name: parsed[i].subject,
+            Amount: parsed[i].amount
+          }
+        });
+      }
+    }
+
+    // Commit the Unit of Work with all the previous registered operations
+    try {
+      const response = await dataApi.commitUnitOfWork(uow);
+      logger.info(JSON.stringify(response));
+    } catch (err) {
+      const errorMessage = `Failed to insert record. Root Cause : ${err.message}`;
+      logger.error(errorMessage);
+      throw new Error(errorMessage);
+    }
+
+    return '{message: "Success"}';
+  });
+  
+}
